79bf9aa5f88942db871698edbd8436c02dc42623
baba-s <shotababa1206@gmail.com>
2022-10-31 13:36:50 +0900

    add

---
+++ Editor/SaveAssetsScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+
+namespace Kogane
+{
+    /// <summary>
+    /// ビルドのために書き換えた設定を元に戻した後にディスクに保存するためのスコープ
+    /// </summary>
+    public sealed class SaveAssetsScope : IDisposable
+    {
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// ビルド終了時に呼び出されます
+        /// </summary>
+        public void Dispose()
+        {
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SaveAssetsScope
+# Kogane Save Assets Scope
+
+ビルドのために書き換えた設定を元に戻した後にディスクに保存するためのスコープ
